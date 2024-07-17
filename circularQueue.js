32fff623c959aefcf37702a0b5d62e7b678c9baa
Muhammed Ziyavudheen <ziyavudheen1234@gmail.com>
2024-07-17 10:49:30 +0530

    added circularQueue,queue

---
+++ circularQueue.js
@@ -0,0 +1,79 @@
+class CircilarQueue {
+  constructor(capacity) {
+    this.items = new Array(capacity);
+    this.capacity = capacity;
+    this.currentLength = 0;
+    this.start = -1;
+    this.end = -1;
+  }
+
+  isFull() {
+    return this.currentLength === this.capacity;
+  }
+
+  isEmpty() {
+    return this.currentLength === 0;
+  }
+
+  enqueue(element) {
+    if (!this.isFull()) {
+      this.end = (this.end + 1) % this.capacity;
+      this.items[this.end] = element;
+      this.currentLength += 1;
+
+      if (this.start === -1) {
+        this.start = this.end;
+      }
+    }
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    const item = this.items[this.start];
+    this.items[this.start] = null;
+    this.start = (this.start + 1) % this.capacity;
+    this.currentLength -= 1;
+
+    if (this.isEmpty()) {
+      this.start = -1;
+      this.end = -1;
+    }
+
+    return item;
+  }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log("Queue is Empty");
+    } else {
+      let i;
+      let str = "";
+      for (i = this.start; i != this.end; i = (i + 1) % this.capacity) {
+        str += this.items[i] + " ";
+      }
+
+      str += this.items[i];
+      console.log(str);
+    }
+  }
+}
+
+const list = new CircilarQueue(5);
+
+list.enqueue(10);
+list.enqueue(20);
+list.enqueue(30);
+list.enqueue(40);
+list.enqueue(50);
+list.enqueue(60);
+
+list.print();
+
+list.dequeue();
+list.dequeue();
+// list.dequeue();
+
+list.print();
---
+++ queue.js
@@ -0,0 +1,85 @@
+/// Queue with Array
+
+// class Queue {
+//   constructor(capacity) {
+//     this.queue = [];
+//     this.capacity = capacity;
+//   }
+
+//   enqueue(element) {
+//     if (this.queue.length < this.capacity) {
+//       this.queue.push(element);
+//     }
+//   }
+
+//   dequeue() {
+//     if (this.queue.length > 0) {
+//       this.queue.shift();
+//     }
+//   }
+
+
+//   print(){
+
+//     console.log(this.queue);
+//   }
+// }
+
+
+// let queue = new Queue(5)
+
+// queue.enqueue(1)
+// queue.enqueue(2)
+// queue.enqueue(3)
+// queue.enqueue(4)
+// queue.enqueue(5)
+
+// queue.print()
+
+// queue.dequeue()
+// queue.print()
+
+
+
+// Queue With Object
+
+class Queue{
+
+    constructor(){
+
+         this.items = {}
+         this.start = 0
+         this.end = 0
+
+    }
+
+    enqueue(element){
+
+        this.items[this.end] = element
+
+         this.end++;
+    }
+
+    dequeue(){
+
+        delete this.items[this.start]
+        this.start++;
+    }
+    display(){
+
+        console.log(this.items);
+    }
+
+}
+
+let queue = new Queue()
+
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+queue.enqueue(40)
+queue.enqueue(50)
+queue.display()
+
+queue.dequeue()
+queue.display()
